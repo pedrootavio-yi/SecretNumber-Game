384e8b48508eebc45a1c837dd6e1298689d2fc7a
pedrootavio-yi <pedrootavio.yi@outlook.com>
2024-06-10 20:18:22 -0300

    subindo os arquivos do projeto

---
+++ app.js
@@ -0,0 +1,8 @@
+alert("Bem-vindo(a) ao Game NumberSecret!");
+
+let numeroSecreto = 16;
+let palpiteDoUsuario = prompt("Eu pensei em um número entre 0 e 20, tente adivinhar qual é...");
+
+if (palpiteDoUsuario == numeroSecreto) {
+    console.log("Isso aí! Você acertou, o numéro que eu estava pensando era o 16");
+};
