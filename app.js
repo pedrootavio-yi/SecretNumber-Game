3a6ba690ad7b0c5d7fccfb7ec7661a3e034931a1
pedrootavio-yi <pedrootavio.yi@outlook.com>
2024-06-17 20:23:38 -0300

    estruturas de repetição

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 alert("Bem-vindo(a) ao Game NumberSecret!");
 
 let numeroSecreto = 10;
-let palpiteDoUsuario = prompt("Eu pensei em um número entre 0 e 10, tente adivinhar qual é...");
+let palpiteDoUsuario;
+let tentativasDoUsuario = 1;
 
-if (palpiteDoUsuario == numeroSecreto) {
-    alert(`Isso aí! Você acertou, o número que eu estava pensando era o número (${numeroSecreto})`);
-} else {
-    alert("Você errou :(");
-};
+while (palpiteDoUsuario != numeroSecreto) {
+    palpiteDoUsuario = prompt("Eu pensei em um número entre 0 e 10, tente adivinhar qual é...");
+
+    if (palpiteDoUsuario == numeroSecreto) {
+        alert(`Isso aí! Você acertou, o número que eu estava pensando era o número (${numeroSecreto}). Total de palpites: ${tentativasDoUsuario}`);
+    } else {
+        if (palpiteDoUsuario > numeroSecreto) {
+            alert(`O valor ${palpiteDoUsuario} é maior que o número que pensei... tente novamente!`);
+        } else {
+            alert(`O valor ${palpiteDoUsuario} é menor que o número que pensei... tente novamente!`);
+        }
+    };
+
+    tentativasDoUsuario++;
+}
